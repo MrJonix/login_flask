47a3bcceaecced3c9f08baec7b54d81d82d1438d
Jonas Thelen <jonas.thelen@icloud.com>
2024-01-20 10:20:38 +0100

    feat: admin

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import hashlib
 app = Flask(__name__)
 
 # Credentials for the login
-valid_username = "admin"
-valid_password = "password"
+valid_credentials = {
+    "user": "password",
+    "admin": "3s1$tB€vö1k€rungW€$ha1bCHFKurzfri5tig"
+}
 
 # Function to generate an MD5 hash
 def generate_md5_hash(data):
@@ -15,21 +17,27 @@ def generate_md5_hash(data):
 def index():
     # Retrieve the session cookie value from the request cookies
     session_cookie = request.cookies.get('session_cookie')
+    
     # Check if the session cookie is present and matches the expected value
-    if session_cookie and session_cookie == generate_md5_hash(valid_username):
+    if session_cookie and any(session_cookie == generate_md5_hash(username) for username in valid_credentials):
+        # Get the actual username from the valid_credentials dictionary
+        username = [k for k, v in valid_credentials.items() if session_cookie == generate_md5_hash(k)][0]
+        
         # User is authenticated, render the desired page (e.g., dashboard)
-        return render_template("success.html", username=valid_username)
+        if(username == "admin"):
+            return render_template("success.html", username=username, admin= "Congratulations! You've successfully conquered the challenges and emerged victorious in the CTF. Well done! 🎉🔐")
+        return render_template("success.html", username=username)
+        
     else:
         # User is not authenticated, render the login page with an error message
         return render_template("login.html", error="")
-
 
 @app.route("/", methods=["POST"])
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    if username == valid_username and password == valid_password:
+    if username in valid_credentials and password == valid_credentials[username]:
         # If credentials are valid, generate an MD5 hash and set it as a session cookie
         session_cookie = generate_md5_hash(username)
         response = make_response(render_template("success.html", username=username))
@@ -37,7 +45,6 @@ def login():
         return response
     else:
         return render_template("login.html", error="Invalid credentials. Please try again.")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
